45bd81d00ed591765491d9fd1148f30e86bedb9e
Eric <ericdandria2@gmail.com>
2022-11-06 21:54:28 +0100

    Init now creates folder structure
    When init is called the folders for the project are created

--- src/data_types/folder_tree.cpp
+++ src/data_types/folder_tree.cpp
@@ -2,9 +2,56 @@
 
 #include "../file_helper.h"
 
+#ifdef _WIN32
+#define SEPARATOR '\\'
+#elif __unix__
+#define SEPARATOR '/'
+#endif
+
 FolderTree::FolderTree(Parts& parts) {
-    for (const auto& part : parts.getParts()) {
-        // TODO: add create path tree structure from parts
+    auto paths = std::unordered_set<std::string>();
+    
+    for (const auto &pair : parts.getParts()) {
+        paths.emplace(pair.second.getPath());
+    }
+
+    this->name = ".";
+    this->fromStrings(paths);
+}
+
+void FolderTree::fromStrings(const std::unordered_set<std::string>& set) {
+    auto paths = std::unordered_map<std::string,std::unordered_set<std::string>>();
+
+    for (const auto& str : set) {
+        std::string aux = str;
+        
+        std::string::size_type pos;
+
+        if (aux != "") {
+            std::string prefix;
+            
+            if ((pos = aux.find(SEPARATOR)) != std::string::npos) {
+                prefix = aux.substr(0, pos);
+                aux = aux.substr(pos+1);
+            } else {
+                prefix = aux;
+                aux = "";
+            }
+
+            if (paths.find(prefix) == paths.end()) {
+                paths[prefix] = std::unordered_set<std::string>();
+            }
+
+            if (aux != "") {
+                paths[prefix].emplace(aux);
+            }
+        }
+    }
+
+    for (const auto& pair : paths) {
+        FolderTree* ptr = new FolderTree(pair.first);
+        ptr->fromStrings(pair.second);
+        this->addFolder(ptr);
     }
 }
 
@@ -23,13 +70,24 @@ bool FolderTree::hasSubFolder(const std::string& folder_name) {
 }
 
 void FolderTree::createFolderRec() {
-    createFolder(FileHelper::composePath(".", this->name));
+    for (FolderTree* node : this->sub_folders) {
+        node->createFolder(this->name);
+    }
 }
 
 void FolderTree::createFolder(const std::string& acc_path) {
-    FileHelper::createFolder(acc_path);
+    std::string new_acc_path = FileHelper::composePath(acc_path, this->name);
+    
+    FileHelper::createFolder(new_acc_path);
 
-    for (FolderTree* nodes : this->sub_folders) {
-        nodes->createFolder(FileHelper::composePath(acc_path, nodes->name));
+    for (FolderTree* node : this->sub_folders) {
+        node->createFolder(new_acc_path);
+    }
+}
+
+FolderTree::~FolderTree() {
+    for (const auto& node : this->sub_folders) {
+        node->~FolderTree();
+        delete node;
     }
 }
--- src/data_types/folder_tree.h
+++ src/data_types/folder_tree.h
@@ -2,6 +2,8 @@
 
 #include <string>
 #include <vector>
+#include <unordered_set>
+
 #include "parts.h"
 
 class FolderTree {
@@ -11,9 +13,11 @@ private:
 
     bool hasSubFolder(const std::string&);
     void createFolder(const std::string&);
+    void fromStrings(const std::unordered_set<std::string>&);
 public:
     FolderTree(const std::string& c_name) : name(c_name) { sub_folders = std::vector<FolderTree*>(); }
     FolderTree(Parts&);
+    ~FolderTree();
     void addFolder(FolderTree*);
     void createFolderRec();
 };
